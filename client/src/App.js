b5b6d31e179e8474ffa576880dd27acef0cc80b5
Vger <vcdelariva@gmail.com>
2018-10-04 23:31:57 -0500

    Test vote api

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Button from "./components/Button";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  testAPI = event => {
+
+    let query = "http://api.votesmart.org/CandidateBio.getBio";
+
+    axios.get(query, {
+      params: {
+        "key": "b255f096d0fcbe52ea0f5eddab340062",
+        "candidateId":9490
+      }
+    }).then(result => {
+      console.log(`API result: ${JSON.stringify(result)}`)
+    })
+
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +30,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button
+          onClick={this.testAPI}
+          style={{ float: "center", marginBottom: 10 }}
+          className={"btn btn-success"}
+        >
+          Test API
+        </Button>
+
       </div>
     );
   }
