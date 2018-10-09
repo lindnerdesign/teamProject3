2ea606a7ed534665158bcf75c184dc1cf7c7bbd6
Lindsay Lindner <llindner@stp-er-10-129-86-194.wireless.umn.edu>
2018-10-08 20:59:19 -0500

    frontend wireframe

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,54 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+// import { Link } from "react-router-dom";
+import "./NavBar.css";
+import {Navbar, Nav, NavItem, Button} from "react-bootstrap";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-const Navbar = props => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">
-      Vote App
-    </Link>
-    <div>
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={
-              window.location.pathname === "/" || window.location.pathname === "/about"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/discover"
-            className={
-              window.location.pathname === "/discover"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Discover
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/search"
-            className={
-              window.location.pathname === "/search"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Search
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+const NavBar = () => (
+
+  <Navbar fluid fixedTop id="nav">
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="/">Vote Now!</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem eventKey={1} href="/">
+        Home
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Saved
+      </NavItem>
+    </Nav>
+
+    <Navbar.Form pullRight>
+    <Button bsStyle="danger">Sign In</Button>
+    </Navbar.Form>
+
+  </Navbar.Collapse>
+</Navbar>
 );
 
-export default Navbar;
+export default NavBar;
---
+++ client/src/components/Podcast/Podcast.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Podcast.css";
+import {Button} from "react-bootstrap";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+const Podcast = () => (
+  
+  <div className="podcast">
+  <h1 className="addressheader text-center">Your Podcasts</h1>
+
+  </div>
+);
+
+export default Podcast;
