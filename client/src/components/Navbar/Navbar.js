86ba0f1890f17d6aa764322d1df13103d13efdda
Vger <vcdelariva@gmail.com>
2018-10-10 21:31:43 -0500

    Fix NavBar references, again...

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./NavBar.css";
 import {Navbar, Nav, NavItem, Button} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
