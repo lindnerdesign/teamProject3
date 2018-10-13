47092c238a2c7c9724ee69ffd095fb3b18a52e63
Lindsay Lindner <llindner@stp-er-10-129-50-124.wireless.umn.edu>
2018-10-13 11:45:18 -0500

    banner fix

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -3,8 +3,8 @@ import "./Hero.css";
 
 const Hero = () => (
   <div className="jumbotron jumbotron-fluid text-center">
-  <div className="container">
-    <img src="../../logo.png" alt="Vote Now"/>
+  <div className="container-fluid">
+    <img src="../../logo.png" alt="Vote Now" />
   </div>
 </div>
 );
