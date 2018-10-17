97669225013adc4942d63cf89aa8e923e3df9229
AbhinavS7 <toabhinavs25@gmail.com>
2018-10-16 21:30:58 -0500

    register component

--- client/src/components/authRegister/Register.js
+++ client/src/components/authRegister/Register.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Register.css';
 import API from "../../utils/API";
 import NavBar from "../NavBar";
-import {Row, Col, Button} from "react-bootstrap";
+import { Row, Col, Button, Grid } from "react-bootstrap";
 
 class Create extends Component {
 
@@ -47,11 +47,11 @@ class Create extends Component {
     API.registerVoter(voter)
       .then((result) => {
         console.log(result);
-        if(result.data.success===false){
+        if (result.data.success === false) {
           this.setState({ message: result.data.msg });
-        }else{
+        } else {
           this.setState({ message: 'Registration Successful. Please Login now' });
-          this.props.history.push("/login")
+          //this.props.history.push("/login")
 
         }
       });
@@ -67,69 +67,99 @@ class Create extends Component {
     return (
 
       <div className="container">
-      <Row className="voteSearch">
-      <NavBar />
-      </Row>
-        <form className="form-signin" onSubmit={this.onSubmit}>
-        {this.state.message !== '' &&
-            <div className="alert alert-warning alert-dismissible" role="alert">
-              { this.state.message }
-            </div>
-          }
-          <h2 className="form-signin-heading">Register</h2>
-          <input type="email" className="form-control" placeholder="Email address" name="username" value={username} onChange={this.handleInputChange} required />
-          <input
-            value={this.state.firstName}
-            onChange={this.handleInputChange}
-            name="firstName"
-            type="text"
-            className="form-control"
-            placeholder="First Name"
-          />
-          <input
-            value={this.state.lastName}
-            onChange={this.handleInputChange}
-            name="lastName"
-            type="text"
-            className="form-control"
-            placeholder="Last Name"
-          />
-          <input
-            value={this.state.line1}
-            onChange={this.handleInputChange}
-            name="line1"
-            type="text"
-            className="form-control"
-            placeholder="Street Address"
-          />
-          <input
-            value={this.state.city}
-            onChange={this.handleInputChange}
-            name="city"
-            type="text"
-            className="form-control"
-            placeholder="City"
-          />
+        <Row className="voteSearch">
+          <NavBar />
+        </Row>
+        <Grid>
+          <Row>
+            <Col xs={12} sm={12} md={12}>
+              <form className="form-signin" onSubmit={this.onSubmit}>
+              <div>
+              {this.state.message !== ''
+              ? <div>
+              {this.state.message.toString().includes('Successful') ?
+                  (<div className="alert alert-success alert-success" role="alert">
+                    {this.state.message}
+                  </div>):(<div className="alert alert-success alert-danger" role="alert">
+                    {this.state.message}
+                  </div>)}
+              </div>
+              :null
+              }
+                {/* {this.state.message.toString().includes('Successful') ?
+                  (<div className="alert alert-success alert-success" role="alert">
+                    {this.state.message}
+                  </div>):
+                  ({this.state.message !== '' ? (<div className="alert alert-success alert-danger" role="alert">
+                    {this.state.message}
+                  </div>):null})} */}
+                  </div>
+                <h2 className="form-signin-heading">Register</h2>
+                <input
+                  type="email"
+                  value={this.state.username}
+                  onChange={this.handleInputChange}
+                  name="username"
+                  type="text"
+                  className="form-control register"
+                  placeholder="Email address"
+                  required
+                />
+                <input
+                  value={this.state.firstName}
+                  onChange={this.handleInputChange}
+                  name="firstName"
+                  type="text"
+                  className="form-control register"
+                  placeholder="First Name"
+                />
+                <input
+                  value={this.state.lastName}
+                  onChange={this.handleInputChange}
+                  name="lastName"
+                  type="text"
+                  className="form-control register"
+                  placeholder="Last Name"
+                />
+                <input
+                  value={this.state.line1}
+                  onChange={this.handleInputChange}
+                  name="line1"
+                  type="text"
+                  className="form-control register"
+                  placeholder="Street Address"
+                />
+                <input
+                  value={this.state.city}
+                  onChange={this.handleInputChange}
+                  name="city"
+                  type="text"
+                  className="form-control register"
+                  placeholder="City"
+                />
 
-          <input
-            value={this.state.state}
-            onChange={this.handleInputChange}
-            name="state"
-            type="text"
-            className="form-control"
-            placeholder="State"
-          />
-          <input
-            value={this.state.zip}
-            onChange={this.handleInputChange}
-            name="zip"
-            type="text"
-            className="form-control"
-            placeholder="ZIP Code"
-          />
-          <input type="password" className="form-control" placeholder="Password" name="password" value={password} onChange={this.handleInputChange} required />
-          <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-        </form>
+                <input
+                  value={this.state.state}
+                  onChange={this.handleInputChange}
+                  name="state"
+                  type="text"
+                  className="form-control register"
+                  placeholder="State"
+                />
+                <input
+                  value={this.state.zip}
+                  onChange={this.handleInputChange}
+                  name="zip"
+                  type="text"
+                  className="form-control register"
+                  placeholder="ZIP Code"
+                />
+                <input type="password" className="form-control register" placeholder="Password" name="password" value={password} onChange={this.handleInputChange} required />
+                <Button className="btn btn-lg btn-primary" type="submit">Register</Button>
+              </form>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
